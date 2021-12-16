486c4d20c74afb943432db5aa33ece23e375eb16
Elang Surya <elangsurya19@gmail.com>
2021-12-16 11:27:04 +0700

    host dump

--- conn.php
+++ conn.php
@@ -9,8 +9,8 @@ if (mysqli_connect_errno()) {
 }
 
 // $host = 'http://localhost/secure-coding-karcis/';
-$host = 'http://' . $_SERVER['HTTP_HOST'] ."/";
-var_dump($host, $_SERVER);exit;
+$host = 'http://' . $_SERVER['HTTP_REFERER'] ."";
+// var_dump($host, $_SERVER);exit;
 error_reporting(E_ALL);
 
 
