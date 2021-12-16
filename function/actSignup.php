a3861cd29c9b42b0fbb391087b16858106250fc1
Elang Surya <elangsurya19@gmail.com>
2021-12-16 14:20:14 +0700

    dump html

--- function/actSignup.php
+++ function/actSignup.php
@@ -1,13 +1,13 @@
 <?php
     include "../conn.php";
 
-    $fullname = mysqli_real_escape_string(@$_POST['fullname']);
-    $email    = mysqli_real_escape_string(@$_POST['email']);
-    $password  = sha1(mysqli_real_escape_string(@$_POST['password']));
+    $fullname = htmlentities(@$_POST['fullname']);
+    $email    = htmlentities(@$_POST['email']);
+    $password  = sha1(htmlentities(@$_POST['password']));
 
     // insert to database
     $sql = "INSERT INTO users (email, password) VALUES ('$email', '$password')";
-
+var_dump($fullname);exit;
     if ($conn->query($sql) === TRUE) {
         $sql_profile = "INSERT INTO user_profile (id_user,fullname) VALUES ('$conn->insert_id','$fullname')";
         if($conn->query($sql_profile) === TRUE){
