75d7af7509fe0b478e088bd02ab90cf7d1823ffa
Elang Surya <elangsurya19@gmail.com>
2021-12-16 10:28:40 +0700

    update host

--- conn.php
+++ conn.php
@@ -8,8 +8,8 @@ if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$host = 'http://localhost/secure-coding-karcis/';
-
+// $host = 'http://localhost/secure-coding-karcis/';
+$host = 'http://' . $_SERVER['HTTP_HOST'] ."/";
 error_reporting(E_ALL);
 
 
